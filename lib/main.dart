1951054389aaa18b0776b99f643a507a3225f2bc
Sanjana <asanjana0823@gmail.com>
2025-02-03 22:42:59 +0530

    Update main page

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'remove content:',
             ),
             Text(
               '$_counter',
