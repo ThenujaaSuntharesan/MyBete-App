2adbe3b2604e201d0d8b6099e87e15251ce6265f
Sanjana <asanjana0823@gmail.com>
2025-02-07 13:46:41 +0530

    Add onboarding items

---
+++ lib/onboarding/onboarding_items.dart
@@ -0,0 +1,26 @@
+
+import 'onboarding_info.dart';
+
+class OnboardingItems{
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "title",
+        descriptions: "descriptions",
+        image: "assets/onboard_img_1.svg"),
+
+    OnboardingInfo(
+        title: "title",
+        descriptions: "descriptions",
+        image: "assets/onboard_img_2.svg"),
+
+    OnboardingInfo(
+        title: "title",
+        descriptions: "descriptions",
+        image: "assets/onboard_img_3.svg"),
+
+    OnboardingInfo(
+        title: "title",
+        descriptions: "descriptions",
+        image: "assets/onboard_img_4.svg")
+  ];
+}
