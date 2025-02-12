453811786a61c4bde556fbf52e0a38b50eb805dd
Sanjana <asanjana0823@gmail.com>
2025-02-12 15:35:37 +0530

    Add firebase cloud database

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mybete_app/diabete_options.dart';
+import 'package:mybete_app/firebase_options.dart';
 import 'package:mybete_app/onboarding/onboarding_view.dart';
 import 'package:mybete_app/src/utils/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
   final onboarding = prefs.getBool("onboarding")??false;
 
