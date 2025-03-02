3d74f5b57f451f7dd1f123c4471330e57491eb86
Sanjana <asanjana0823@gmail.com>
2025-03-03 00:12:03 +0530

    Update diabetes options

--- lib/diabete_options.dart
+++ lib/diabete_options.dart
@@ -7,14 +7,23 @@ import 'donot_have_diabetes/donot_have_diabete.dart';
 import 'have_diabetes/have_diabete.dart';
 import 'not_sure/not_sure.dart';
 
-
 class DiabeteOptions extends StatelessWidget {
   const DiabeteOptions({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Diabetes Options")), // Add an app bar
+      appBar: AppBar(
+        title: Text("Diabetes Options"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle, size: 50), // Increase the size of the icon
+            onPressed: () {
+              _showLogoutMenu(context);
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -31,10 +40,35 @@ class DiabeteOptions extends StatelessWidget {
             SizedBox(height: 20),
             doNotHaveDiabetes(context),
             SizedBox(height: 20),
-            notSure(context),// Correctly use the button here
+            notSure(context), // Correctly use the button here
           ],
         ),
       ),
+    );
+  }
+
+  void _showLogoutMenu(BuildContext context) {
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(1000, 80, 0, 0),
+      items: [
+        PopupMenuItem(
+          child: ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Log Out'),
+            onTap: () async {
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.setBool('isLoggedIn', false);
+              await prefs.remove('lastLoginDate');
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+                    (route) => false,
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -107,4 +141,3 @@ Widget notSure(BuildContext context) {
     ),
   );
 }
-
