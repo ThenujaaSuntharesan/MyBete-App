65f75035210970ad81cbf444933042bd80aff4d9
Shaleeka2002 <164978139+Shaleeka2002@users.noreply.github.com>
2025-03-04 03:10:01 +0530

    add first page of meal planner

---
+++ lib/donot_have_diabetes/meal_plans/meal.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AppNewBoldCollection {
+  const AppNewBoldCollection._();
+
+  // Font family for Bold
+  static const String fontFamily = 'Bold';
+
+  static List<Map<String, dynamic>> get map => [
+    {'fontFamily': fontFamily},
+  ];
+}
+
+class AppBoldCollection {
+  const AppBoldCollection._();
+
+  // Font family for Bold
+  static const String fontFamily = 'Bold';
+
+  static List<Map<String, dynamic>> get map => [
+    {'fontFamily': fontFamily},
+  ];
+}
+
+class AppRegularCollection {
+  const AppRegularCollection._();
+
+  // Font family for Regular
+  static const String fontFamily = 'Regular';
+
+  static List<Map<String, dynamic>> get map => [
+    {'fontFamily': fontFamily},
+  ];
+}
+
+class AppNewCollection {
+  const AppNewCollection._();
+
+  // Font family for Regular
+  static const String fontFamily = 'Regular';
+
+  static List<Map<String, dynamic>> get map => [
+    {'fontFamily': fontFamily},
+  ];
+}
+
+class AppNewCollectionBold {
+  const AppNewCollectionBold._();
+
+  // Font family for Bold
+  static const String fontFamily = 'Bold';
+
+  static List<Map<String, dynamic>> get map => [
+    {'fontFamily': fontFamily},
+  ];
+}
