56dcc6736f0448be599bb704afb239a5620383a9
ThibakarSri <thibakarsri33@gmail.com>
2025-03-23 13:01:41 +0530

    retrive the merged files

--- lib/have_diabetes/DashBoard/MyActivity/MyActivity.dart
+++ lib/have_diabetes/DashBoard/MyActivity/MyActivity.dart
@@ -5,7 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'dart:math';
-import '../Reminder/Reminders.dart';
+import '../Reminder/Reminder_screen.dart';
 import '../Report/report_screen.dart';
 import 'LogDetails/LogInterface.dart';
 import 'log_provider.dart';
