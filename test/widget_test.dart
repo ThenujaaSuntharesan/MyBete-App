8a6c8c10f38e52296f1249348bf737f80af92dee
Sanjana <asanjana0823@gmail.com>
2025-04-05 13:15:34 +0530

    Change widget test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,37 +1,37 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mybete_app/diabete_options.dart';
 import 'package:mybete_app/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:mocktail/mocktail.dart';
-
-// Mock SharedPreferences
-class MockSharedPreferences extends Mock implements SharedPreferences {}
 
 void main() {
   group('MyApp Widget Test', () {
-    late MockSharedPreferences mockPrefs;
-
-    setUp(() {
-      mockPrefs = MockSharedPreferences();
-    });
-
     testWidgets('Should display CircularProgressIndicator while checking login status', (WidgetTester tester) async {
-      // Mock that user is logged out
-      when(() => mockPrefs.getBool('isLoggedIn')).thenReturn(false);
-      when(() => mockPrefs.getString('lastLoginDate')).thenReturn('');
+      // Fake SharedPreferences for logged out state
+      SharedPreferences.setMockInitialValues({
+        'isLoggedIn': false,
+        'lastLoginDate': '',
+      });
 
       await tester.pumpWidget(MyApp());
 
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      // Let async init finish
+      await tester.pumpAndSettle();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     });
 
     testWidgets('Should display DiabeteOptions when user is logged in', (WidgetTester tester) async {
-      // Mock that user is logged in
-      when(() => mockPrefs.getBool('isLoggedIn')).thenReturn(true);
-      when(() => mockPrefs.getString('lastLoginDate')).thenReturn(DateTime.now().toIso8601String());
+      // Fake SharedPreferences for logged in state
+      SharedPreferences.setMockInitialValues({
+        'isLoggedIn': true,
+        'lastLoginDate': DateTime.now().toIso8601String(),
+      });
 
       await tester.pumpWidget(MyApp());
+
+      // Let async init finish
+      await tester.pumpAndSettle();
 
       expect(find.byType(DiabeteOptions), findsOneWidget);
     });
